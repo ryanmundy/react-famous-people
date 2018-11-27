ade2de07f84e593e58566edea46c494d8ed5041f
Ryan Mundy <ryanmundy34@gmail.com>
2018-11-27 16:40:20 -0600

    inputs are added and displaying on DOM

--- src/App.js
+++ src/App.js
@@ -3,26 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state =
+      {
+        famousPerson: {
+          name: '',
+          role: ''
+        }
+      }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <h1 className="Header">React-Famous-People</h1>
+      <input type="text" placeholder="name" onChange={this.handleNameChange}/>
+      <input type="text" placeholder="role" onChange={this.handleRoleChange} />
+        <p>{this.state.famousPerson.name} is famous for "{this.state.famousPerson.role}"</p>
       </div>
     );
   }
+
+handleNameChange=(event)=>{
+  this.setState(
+    {
+      famousPerson: {
+        ...this.state.famousPerson,
+        name: event.target.value
+      }
+    }
+  )
 }
+
+  handleRoleChange = (event) => {
+    this.setState(
+      {
+        famousPerson: {
+          ...this.state.famousPerson,
+          role: event.target.value
+        }
+      }
+    )
+  }
+
+
+}
+
 
 export default App;
